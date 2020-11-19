1838933b3dcde5a02b77125d73b72d64344ac215
dryans1 <dryans@naver.com>
2020-11-20 00:40:55 +0900

    basic css setup, movies.html

--- future_to_the_back/config/settings.py
+++ future_to_the_back/config/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'config'/ 'static', 
+]
+
 
 
 AUTH_USER_MODEL = 'accounts.User'
