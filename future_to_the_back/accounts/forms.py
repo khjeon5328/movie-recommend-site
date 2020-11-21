65875b3606bcf29f0df922ab11db2fda0ebdf7d5
khjeon5 <khjeon5328@naver.com>
2020-11-21 14:05:22 +0900

    work in accounts app

---
+++ future_to_the_back/accounts/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import User
+
+
+class UserCreationForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password',)
