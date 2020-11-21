cec3a1eafaf1f11eb9a9a683e64ed2da6402866a
khjeon5328 <khjeon5328@naver.com>
2020-11-21 21:52:48 +0900

    finish accounts app

--- future_to_the_back/accounts/models.py
+++ future_to_the_back/accounts/models.py
@@ -2,4 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='followings')
+    
+    def __str__(self):
+        return self.username
--- future_to_the_back/accounts/urls.py
+++ future_to_the_back/accounts/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('<username>/', views.profile, name='profile'),
+    path('update/', views.update, name='update'),
+    path('password/', views.change_password, name='change_password'),
+    path('<str:username>/', views.profile, name='profile'),
+    path('<str:username>/follow/', views.follow, name='follow'),
 ]
