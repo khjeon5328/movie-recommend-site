18c9f9e7e55338ecd5c93973f56a47ca19c65219
khjeon5 <khjeon5328@naver.com>
2020-11-23 11:50:37 +0900

    add 1123 morning

--- future_to_the_back/reviews/views.py
+++ future_to_the_back/reviews/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Review
-from .forms import ReviewForm
-
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
+from .models import (
+    Review, 
+    Comment
+)
+from .forms import (
+    ReviewForm, 
+    CommentForm
+)
+import json
+from django.http import JsonResponse
 # Create your views here.
+
 
 def reviews(request):
     reviews = Review.objects.all()
@@ -11,6 +21,9 @@ def reviews(request):
     }
     return render(request, 'reviews/reviews.html', context)
 
+
+@login_required
+@require_http_methods(['GET', 'POST'])
 def create_review(request):
     if request.method == 'POST':
         form = ReviewForm(request.POST, request.FILES)
@@ -26,10 +39,13 @@ def create_review(request):
     }
     return render(request, 'reviews/create.html', context)
 
+
+@login_required
+@require_http_methods(['GET'])
 def detail(request, review_id):
     review = get_object_or_404(Review, pk=review_id)
     comment_form = CommentForm()
-    comments = article.comment_set.all()
+    comments = review.comment_set.all()
     context = {
         'review': review,
         'comment_form' : comment_form,
@@ -38,13 +54,16 @@ def detail(request, review_id):
     return render(request, 'reviews/detail.html', context)
 
 
+@login_required
+@require_http_methods(['GET', 'POST'])
 def update(request, review_id):
     review = get_object_or_404(Review, pk=review_id)
-    if request.method == 'POST':
-        form = ReviewForm(request.POST, instance=review)
-        if form.is_valid():
-            form.save()
-            return redirect('reviews:detail', review_id)
+    if request.user == review.author:
+        if request.method == 'POST':
+            form = ReviewForm(request.POST, instance=review)
+            if form.is_valid():
+                form.save()
+                return redirect('reviews:detail', review_id)
     else:
         form = ReviewForm(instance=review)
     context = {
@@ -52,11 +71,48 @@ def update(request, review_id):
     }
     return render(request, 'reviews/update.html', context)
 
+
+@login_required
+@require_http_methods(['POST'])
 def delete(request, review_id):
-    get_object_or_404(Review, pk=review_id).delete()
+    review = get_object_or_404(Review, pk=review_id)
+    if request.user == review.author:
+        review.delete()
     return redirect('reviews:reviews')
 
 
+@login_required
+@require_http_methods(['POST'])
 def create_comment(request, review_id):
     review = get_object_or_404(Review, pk=review_id)
-    
+    comment_form = CommentForm(json.loads(request.body.decode('utf-8')))
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.review = review
+        comment.author = request.user
+        comment.save()
+        data = {
+            'content' : comment.content,
+            'author' : str(comment.author),
+            'count' : review.comment_set.count(),
+        }
+        return JsonResponse(data)
+    context = {
+        'comment_form': comment_form,
+        'review': review,
+    }
+    return render(request, 'reviews/detail.html', context)
+
+
+def delete_comment(request, review_id, comment_id):
+    if request.user.is_authenticated:
+        comment = get_object_or_404(Comment, pk=comment_id)
+        review = get_object_or_404(Review, pk=review_id)
+        if request.user == comment.author:
+            comment.delete()
+            data = {
+                'is_delete' : True,
+                'count': review.comment_set.count(),
+            }
+            return JsonResponse(data)
+    return redirect('reviews:detail', review_id)
