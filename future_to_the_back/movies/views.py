df706b4ac7440eb18a5dafcc8b90da29ef9843f9
khjeon5 <khjeon5328@naver.com>
2020-11-24 10:58:24 +0900

    recommend movie

--- future_to_the_back/movies/views.py
+++ future_to_the_back/movies/views.py
@@ -25,8 +25,19 @@ def movies(request):
     for i in movies:
         movies_detail.append(MovieDetail.objects.get(movie=i))
 
+    first_rank_movie_score = movies_detail[0].netizen_score + movies_detail[0].special_score
+    first_rank_movie_idx = 0
+    for i in range(1, len(movies_detail)):
+        score = movies_detail[i].netizen_score + movies_detail[i].special_score
+        if score > first_rank_movie_score:
+            first_rank_movie_score = score
+            first_rank_movie_idx = i
+    
+
     context = {
         'movies_detail' : movies_detail,
+        'hidden_movie' : movies_detail[first_rank_movie_idx],
+        'rank_1_movie_title' : movies_detail[0].movie.movie_name,
     }
     return render(request, 'movies/movies.html', context)
 
@@ -36,6 +47,7 @@ def moviedetail(request, movie_id):
     movie_detail = MovieDetail.objects.filter(pk=movie_id)
     movie = MovieDetail.objects.get(pk=movie_id)
     reviews = movie.review_set.all()
+ 
 
     context = {
         'movie_detail':movie_detail[0],
