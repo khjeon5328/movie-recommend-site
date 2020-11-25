af7e9c27eb1602199feaf4af79b43a07291cd6cb
khjeon5 <khjeon5328@naver.com>
2020-11-25 22:07:10 +0900

    url handling

--- future_to_the_back/config/urls.py
+++ future_to_the_back/config/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', include('movies.urls')),
-    path('', include('movies.urls')),
+    path('', lambda req: redirect('/movies/')),
+    # path('', include('movies.urls')),
     path('reviews/', include('reviews.urls')),
     path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
